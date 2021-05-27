06d5d5450cbee475112fbf259037ce1545d3d3a4
Kunal Arora <10kunalji@gmail.com>
2021-05-27 15:21:27 +0530

    Working on app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+// import "App.css"
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import Home from './Pages/Home'
+import Signup from './Pages/SignUp'
+import Login from './Pages/LogIn'
+import NotFound from './Pages/NotFound'
+import {UserContext} from './Context/UserContext'
 
-function App() {
+
+const App = () => {
+
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{user, setUser}} >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="*" component={NotFound} />
+        </Switch>
+      </UserContext.Provider>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Pages/LogIn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogIn = () => {
+    return (
+        <h1>Login Page</h1>
+    )
+}
+
+export default LogIn
